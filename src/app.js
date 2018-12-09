64e5108560092b6a9bfcfe9e62161fff49cef0dd
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-09 12:44:16 +0100

    Section 3, Lecture 12

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ console.log('App.js is running');
 
 const app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in hands of a computer.'
+  subtitle: 'Put your life in hands of a computer.',
+  options: ['One', 'Two'],
 }
 
 const template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
     <ol>
       <li>Item 1</li>
       <li>Item 2</li>
@@ -24,11 +26,17 @@ const user = {
   location: 'Paris',
 }
 
+const getLocation = (location) => {
+  if (location) {
+    return <p>Location: {location}</p>
+  }
+}
+
 const templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
   </div>
 );
 
