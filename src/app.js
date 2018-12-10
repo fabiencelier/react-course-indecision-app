9df1e9ea82201d40a3d0d34822d6858c63d54406
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-10 13:29:10 +0100

    Section 3, Lecture 18

--- src/app.js
+++ src/app.js
@@ -5,54 +5,46 @@ console.log('App.js is running');
 const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in hands of a computer.',
-  options: ['One', 'Two'],
+  options: [],
 }
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item 1</li>
-      <li>Item 2</li>
-    </ol>
-  </div>
-);
-
-let count = 0;
-
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
-
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-
-const reset= () => {
-  count = 0;
-  renderCounterApp();
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  console.log(option)
+  if(option){
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
 }
 
-
+const onRemoveAll = (e) => {
+  app.options = [];
+  render();
+}
 
 const appRoot = document.getElementById('app');
 
-
-const renderCounterApp = () => {
-  const templateTwo = (
+const render = () => {
+  const template = (
     <div>
-      <h1>Count : {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
   );
-
-  ReactDOM.render(templateTwo, appRoot);
+  ReactDOM.render(template, appRoot);
 }
 
-renderCounterApp();
+render();
