76d32b0a9b7cd1f1d5b430248d4c10620e5de2ef
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-10 13:41:10 +0100

    Section 3, Lecture 19

--- src/app.js
+++ src/app.js
@@ -35,8 +35,9 @@ const render = () => {
       <p>{app.options.length}</p>
       <button onClick={onRemoveAll}>Remove All</button>
       <ol>
-        <li>Item 1</li>
-        <li>Item 2</li>
+        {
+          app.options.map(opt => <li key={opt}>{opt}</li>)
+        }
       </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
