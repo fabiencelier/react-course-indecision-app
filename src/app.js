2bc8f6c95735338507a98f0447920dbcb5ebe936
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-16 17:22:35 +0100

    Section 6, Lecture 51

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('running !');
+// import './util'
+import { square, add } from './util'
+import { isAdult, canDrink } from './person';
+
+console.log('App running !');
+console.log(square(20));
+console.log(add(55,45));
+console.log(canDrink(22));
+console.log(isAdult(15))
