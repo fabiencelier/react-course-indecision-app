b4dc41f7f7b214f6643ec5feec4af4ada098e1ab
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-16 17:40:13 +0100

    Section 6, Lecture 53

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-// import './util'
-import subtract, { square, add } from './util'
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('App running !');
-console.log(square(20));
-console.log(add(55,45));
-console.log(subtract(10,4));
-
-console.log(canDrink(22));
-console.log(isAdult(15));
-console.log(isSenior(65));
+const template = React.createElement('p', {}, 'test');
+ReactDOM.render(template, document.getElementById('app'));
