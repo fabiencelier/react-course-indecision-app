a2f77d32a037a5eb9bfb2c767041ffedeefe118d
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-16 22:00:32 +0100

    Section 7, Lecture 62

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Option = (props) => {
-  return (
-    <div>
-      <p>{props.option}
-        <button
-          onClick={(e) => props.handleRemoveOption(props.option)}
-        >
-          Remove
-        </button>
-      </p>
-    </div>
-  )
-}
+const Option = (props) => (
+  <div>
+    <p>{props.option}
+      <button
+        onClick={(e) => props.handleRemoveOption(props.option)}
+      >
+        Remove
+      </button>
+    </p>
+  </div>
+);
 
 export default Option;
