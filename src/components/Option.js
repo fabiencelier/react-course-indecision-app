8edd144cdacb7f84f47331d04b71fc8db435d821
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-23 10:44:34 +0100

    Section 8, Lecture 68

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
   <div>
     <p>{props.option}
       <button
+        className='button button--link'
         onClick={(e) => props.handleRemoveOption(props.option)}
       >
         Remove
