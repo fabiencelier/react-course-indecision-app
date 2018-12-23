768cede0671db2d410cb68888d85c76b10166b91
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-23 13:42:25 +0100

    Section 8, Lecture 71

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     contentLabel={'Selected Option'}
     onRequestClose={props.handleClearSelectedState}
+    closeTimeoutMS={200}
+    className='modal'
   >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.handleClearSelectedState}>Ok.</button>
+      <h3 className='modal__title'>Selected Option</h3>
+      {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+      <button className='button' onClick={props.handleClearSelectedState}>Ok.</button>
   </Modal>
 );
 
